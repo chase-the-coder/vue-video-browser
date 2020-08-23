efb8db74b8801a922d096432f9ecc614c72b4073
chase lindsay <chase.lindsay1@gmail.com>
2020-08-23 19:57:51 -0400

    integrated youtube api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>Hi There!</div>
+  <div>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+const API_KEY = "AIzaSyDQF0D4n1-qZw8AZiS6eZ7E13DSBfbPvVo";
 export default {
   name: "App",
+  components: {
+    SearchBar,
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm,
+          },
+        })
+        .then((response) => console.log(response));
+    },
+  },
 };
 </script>
